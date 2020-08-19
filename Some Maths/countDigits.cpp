527fda5f64847b15c32a38e1210ea0e65b1369cc
NiK <nitinkumardj@live.com>
2020-08-19 18:38:29 +0530

    count num of digits

--- Some Maths/countDigits.cpp
+++ Some Maths/countDigits.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <cmath>
 
+void countDigits(int num)
+{
+    int digits{ 0 };
+
+    digits = floor(log10(num) + 1);
+
+    std::cout << digits << '\n';
+}
+
+
+int main()
+{
+    int num{ 12345 };
+
+    countDigits(num);
+}
